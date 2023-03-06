55a169da525535838abf768deb469e662aa36594
JSH <122770834+Bogol-E@users.noreply.github.com>
2023-03-06 18:34:30 +0900

    Update solution.js
    ㅜ

--- solution.js
+++ solution.js
@@ -1,3 +1,8 @@
+// 문제풀이 접근이 잘못된듯하여 말할수있는 단어를 안만들고
+// 입력받은 배열을 돌며 4단어로 스플릿하고 다시 문자열로
+// 합쳐서 ""이면 answer++해서 풀엇는데
+// 밑에 재귀로 단어만들때 브레이크가 안먹히네요
+
 let baby = ["aya", "ye", "woo", "ma", ""];
 let babyable = [];
 let count = 0;
@@ -6,8 +11,7 @@ function make(arr) {
     for (let a of arr) {
         for (let i = 0; i < baby.length; i++) {
             if (a != baby[i]) babyable[count++] = a + baby[i];
-            if (count > 5*5*5*5) break;
-            make(babyable);
+            if (count < 5*5*5*5) make(babyable);
 
         }
     }
